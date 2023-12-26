fd06119cf3e3e416fe71e9a54e6394d0568a1e58
Riku Nakagawa <soccre0409@icloud.com>
2023-12-26 20:55:21 +0900

    create event form and table with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { useEffect, useState } from "react"
 
-
-const App = (props) => {
-
-  const [state, setState] = useState(props)
-  const { name, price } = state
-
-  useEffect(() => {
-    console.log("useEffect is invoked.")
-  })
-
-  const renderPeriod = () => {
-    console.log("renderPeriod renders period")
-    return "。"
-  }
-
+const App = () => {
 
   return (
-    <>
-      <p>現在の{name}は{price}です{renderPeriod()}</p>
-      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
-      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button >
-      <button onClick={() => setState(props)}>reset</button>
-      <input value={name} onChange={(e) => setState({ ...state, name: e.target.value })} />
-    </>
+    <p>This is nakagawa</p>
   )
-}
-
-App.defaultProps = {
-  name: "",
-  price: 1000
 }
 
 export default App
