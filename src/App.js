dbc1aa2c5348fcf0586d359d26a3fbbdbce4795f
Riku Nakagawa <soccre0409@icloud.com>
2023-12-26 19:29:28 +0900

    creat react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react"
 
 
-const App = () => {
+const App = (props) => {
+
+  const [state, setState] = useState(props)
+  const { name, price } = state
+
+  useEffect(() => {
+    console.log("useEffect is invoked.")
+  })
+
+  const renderPeriod = () => {
+    console.log("renderPeriod renders period")
+    return "。"
+  }
+
+
   return (
-    <div>
-      This is a tempale for react app
-    </div>
+    <>
+      <p>現在の{name}は{price}です{renderPeriod()}</p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button >
+      <button onClick={() => setState(props)}>reset</button>
+      <input value={name} onChange={(e) => setState({ ...state, name: e.target.value })} />
+    </>
   )
+}
+
+App.defaultProps = {
+  name: "",
+  price: 1000
 }
 
 export default App
